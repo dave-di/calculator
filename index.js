3cf4d65ed89e2b0a2449acadb2eea84fec5b88c5
Dave Diaz <dave.dz@outlook.com>
2025-05-26 07:06:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ function calculate() {
         display.value = "Error";
     }
 }
+
+const calculatorContainer = document.getElementById('calculator');
+
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+calculatorContainer.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offsetX = e.clientX - calculatorContainer.offsetLeft;
+    offsetY = e.clientY - calculatorContainer.offsetTop;
+    calculatorContainer.style.cursor = 'grabbing';
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    calculatorContainer.style.cursor = 'grab';
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        calculatorContainer.style.left = (e.clientX - offsetX) + 'px';
+        calculatorContainer.style.top = (e.clientY - offsetY) + 'px';
+    }
+});
